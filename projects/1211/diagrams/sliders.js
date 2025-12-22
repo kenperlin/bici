a524af1a0d0aa67a6459164cee3c81ca313d56d1
Ken Perlin <ken.perlin@gmail.com>
2025-12-22 12:19:02 -0500

    Worked on implementation of sliders in diagrams.

--- projects/1211/diagrams/sliders.js
+++ projects/1211/diagrams/sliders.js
@@ -1,28 +1,41 @@
 function Diagram() {
    let names = 'red,green,x,y'.split(',');
-   let sy = n => .9 - .18 * n, sliders = [];
+   let sw = 1.2, sh = .14, y = 1 - .7 * sh, sliders = [];
    for (let n = 0 ; n < names.length ; n++) {
       codeArea.setVar(names[n], .5);
-      sliders.push({ name: names[n], value: .5 });
+      sliders.push({ x: 0, y: y, w: sw, h: sh, name: names[n], value: .5 });
+      y -= 1.2 * sh;
    }
-   let slider = (x,y,w, name, t) => {
-      this.fillColor('black'  ).fillRect([x-w/2-.01,y-.08],[x+w/2+.01,y+.08])
-          .fillColor('white'  ).fillRect([x-w/2    ,y-.07],[x+w/2    ,y+.07])
-          .fillColor('#b0b0b0').fillRect([x-w/2    ,y-.07],[x-w/2+w*t,y+.07])
-          .drawColor('black'  ).text(name, [x, y]);
+   let drawSlider = S => {
+      let e = S.h / 20;
+      let w = S.w * S.value;
+      this.font((200 * S.h) + 'px Helvetica');
+      this.fillColor('black'  ).fillRect([S.x-S.w/2-e,S.y-S.h/2-e],[S.x+S.w/2+e,S.y+S.h/2+e])
+          .fillColor('white'  ).fillRect([S.x-S.w/2  ,S.y-S.h/2  ],[S.x+S.w/2  ,S.y+S.h/2  ])
+          .fillColor('#b0b0b0').fillRect([S.x-S.w/2  ,S.y-S.h/2  ],[S.x-S.w/2+w,S.y+S.h/2  ])
+          .drawColor('black'  ).text(S.name, [S.x, S.y]);
    }
-   this.onDrag = (x,y) => {
+   let S = null;
+   this.onDown = (x,y) => {
       for (let n = 0 ; n < sliders.length ; n++)
-         if (Math.abs(y - sy(n)) < .08) {
-	    sliders[n].value = Math.max(0, Math.min(1, (.4 + x) / .8));
-	    codeArea.setVar(sliders[n].name, sliders[n].value);
+         if (Math.abs(y - sliders[n].y) < sliders[n].h / 2) {
+            S = sliders[n];
+	    S.value = Math.max(0, Math.min(1, .5 + (x - S.x) / S.w));
 	 }
+   }
+   this.onUp = (x,y) => S = null;
+   this.onDrag = (x,y) => {
+      if (S)
+	 S.value = Math.max(0, Math.min(1, .5 + (x - S.x) / S.w));
    }
    this.update = ctx => {
       this.fillColor('white').fillRect([-1,-1],[1,1]);
       for (let n = 0 ; n < sliders.length ; n++) {
-         sliders[n].value = parseFloat(codeArea.getVar(names[n]));
-         slider(0, sy(n), .8, sliders[n].name, sliders[n].value);
+         if (sliders[n] == S)
+	    codeArea.setVar(S.name, S.value);
+         else
+            sliders[n].value = parseFloat(codeArea.getVar(names[n]));
+         drawSlider(sliders[n]);
       }
    }
 }
