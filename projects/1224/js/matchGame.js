75e71f94c63ec64267403e9d4737a815aa723f0c
Ken Perlin <ken.perlin@gmail.com>
2025-12-28 21:59:26 -0500

    Use the sessions's roomId as a random seed to create a unique matchGame.

--- projects/1224/js/matchGame.js
+++ projects/1224/js/matchGame.js
@@ -1,26 +1,36 @@
 function MatchGame(scene,S,A,X) {
-   let sync = () => {
-      codeArea.setVar('S', S);
-      codeArea.setVar('A', A);
-      codeArea.setVar('X', X);
-   }
+   let seed = webrtcClient.roomId.charCodeAt(0) +
+              webrtcClient.roomId.charCodeAt(1) / 128;
+   let label = ['A','B','C','D','E','F','G','H',
+                'A','B','C','D','E','F','G','H'];
+   for (let k = 1 ; k < 100 ; k++)
+   for (let i = 0 ; i < 16 ; i++)
+      if (noise(k+.5, i+.5, seed) > 0) {
+         let j = (i + k) % 16;
+         let tmp  = label[i];
+         label[i] = label[j];
+         label[j] = tmp;
+      }
+
    scene.onUp = (x,y) => {
       for (let n = 0 ; n < 16 ; n++)
          if ( Math.abs(x - tx(n)) < .2 &&
               Math.abs(y - ty(n)) < .2 && ! X[n]) {
             S[n] = 1 - S[n];
             for (let i = 0 ; i < 16 ; i++)
-	       if (i != n && S[i] && ch.charAt(i) == ch.charAt(n))
+	       if (i != n && S[i] && label[i] == label[n])
 	          X[i] = X[n] = 1;
             for (let i = 0 ; i < 16 ; i++)
-	       if (i != n && ! X[i] && ch.charAt(i) != ch.charAt(n))
+	       if (i != n && ! X[i] && label[i] != label[n])
 	          S[i] = 0;
          }
-      sync();
+      codeArea.setVar('S', S);
+      codeArea.setVar('A', A);
+      codeArea.setVar('X', X);
    }
-   let ch = 'ABCDEFGHEDCGHBFA', tile = [];
-   for (let n = 0 ; n < ch.length ; n++)
-      tile.push(createTile(ch.substring(n,n+1)));
+   let tile = [];
+   for (let n = 0 ; n < 16 ; n++)
+      tile.push(createTile(label[n]));
    let tx = n => ((n&3)  - .25) * .4 - .50;
    let ty = n => ((n>>2) - .25) * .4 - .65;
 
@@ -58,7 +68,7 @@ function MatchGame(scene,S,A,X) {
       if (score == 0)
          message('TRY TO FIND\nALL MATCHING PAIRS\nOF TILES.');
       if (score == 32)
-         message('CONGRATULATIONS\nYOU FOUND\nEVERY MATCH!');
+         message('CONGRATULATIONS!\nYOU FOUND\nEVERY MATCH.');
    }
 }
 
