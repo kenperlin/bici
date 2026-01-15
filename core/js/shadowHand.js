c026a0237fdaf599d8a34b7c49f228a4a4e30b94
Ken Perlin <ken.perlin@gmail.com>
2026-01-15 06:22:08 -0500

    Improved shadowHand finger thickness algorithm, and added comments.

--- core/js/shadowHand.js
+++ core/js/shadowHand.js
@@ -1,32 +1,41 @@
 
-window.zMin =  1000;
-window.zMax = -1000;
 let drawShadowHand = (ctx, F, x=0, y=0, s=1) => {
-   
-   let scanvas = ctx.shadowCanvas;
-   if (! scanvas) {
-      scanvas = document.createElement('canvas');
-      document.body.appendChild(scanvas);
-      scanvas.style = '{position:absolute;left:-2000px}';
-      scanvas.width = ctx.canvas.width;
-      scanvas.height = ctx.canvas.height;
-      ctx.shadowCanvas = scanvas;
+
+   // Behind the scenes, create a separate shadow canvas.
+
+   if (! ctx.shadowCanvas) {
+      ctx.shadowCanvas = document.createElement('canvas');
+      document.body.appendChild(ctx.shadowCanvas);
+      ctx.shadowCanvas.style = '{position:absolute;left:-2000px}';
+      ctx.shadowCanvas.width = ctx.canvas.width;
+      ctx.shadowCanvas.height = ctx.canvas.height;
    }
-   let sctx = scanvas.getContext('2d');
-   let w = scanvas.width, h = scanvas.height;
+
+   // Clear the shadow canvas before drawing the hand.
+
+   let sctx = ctx.shadowCanvas.getContext('2d');
+   let w = ctx.canvas.width, h = ctx.canvas.height;
    sctx.clearRect(0,0,w,h);
 
-   let z = 0;
-   for (n = 1 ; n <= 20 ; n++)
-      z -= F[n].z;
+   // Scale finger thickness depending on visible hand size.
 
-   let rScale = .017 * s * w * Math.sqrt(z);
+   let t = 0;
+   for (n = 1 ; n <= 20 ; n += 4)     // loop over 5 fingers
+   for (let i = 0 ; i < 3 ; i++) {    // loop over 3 finger joints
+      let x = F[n+i+1].x - F[n+i].x;
+      let y = F[n+i+1].y - F[n+i].y;
+      let z = F[n+i+1].z - F[n+i].z;
+      t += Math.sqrt(x*x + y*y + z*z);
+   }
+   let handSize = .017 * s * w * (t < 1 ? Math.sqrt(t) : t);
+
+   // Draw an opaque shadow of the hand to the shadow canvas.
 
    for (n = 1 ; n <= 20 ; n += 4) {
-      let r = rScale * (n < 6 ? 1.1 : n < 11 ? 1 : .85);
+      let r = handSize * (n < 6 ? 1.1 : n < 11 ? 1 : .85);
       sctx.fillStyle = ctx.strokeStyle = 'black';
       sctx.lineWidth = 2 * r;
-      let i0 = n > 1 ? 0 : 1;
+      let i0 = n > 1 ? 0 : 1;         // skip first thumb joint
       for (let i = i0 ; i < 4 ; i++) {
          if (i > i0) {
             sctx.beginPath();
@@ -41,6 +50,9 @@ let drawShadowHand = (ctx, F, x=0, y=0, s=1) => {
          }
       }
    }
+
+   // Copy the shadow transparently onto the target canvas.
+
    ctx.globalAlpha = 0.3;
    ctx.drawImage(ctx.shadowCanvas, 0,0);
    ctx.globalAlpha = 1.0;
