201077931cce4f6fb1dd74daf5f6c37ed865aad3
Ken Perlin <ken.perlin@gmail.com>
2026-05-23 15:21:14 -0400

    Use shiny fragment shader in webglCard in project 0423.

--- projects/0423/js/webglCard.js
+++ projects/0423/js/webglCard.js
@@ -2,12 +2,12 @@ function WebglCard(ctx) {
 
    let ball = Shape.sphereMesh(20,10);
    let tube = Shape.glue(Shape.diskMesh(20,-1),
-	      Shape.glue(Shape.tubeMesh(20),
-	                 Shape.diskMesh(20,1)));
+              Shape.glue(Shape.tubeMesh(20),
+                         Shape.diskMesh(20,1)));
    let cube = Shape.cubeMesh();
 
    this.vertexShader = Shader.defaultVertexShader;
-   this.fragmentShader = Shader.defaultFragmentShader;
+   this.fragmentShader = Shader.shinyFragmentShader;
 
    autodraw = false;
 
@@ -17,13 +17,13 @@ function WebglCard(ctx) {
 
    let M = new Matrix();
 
-   let draw = (mesh, color) => {
+   M.draw = (mesh, color) => {
       let m = mxm(perspective(0,0,-5), M.get());
       setUniform('Matrix4fv', 'uMF', false, m);
       setUniform('Matrix4fv', 'uMI', false, inverse(m));
       setUniform('3fv', 'uColor', color ?? [1,1,1]);
       drawMesh(mesh);
-      return this;
+      return M;
    }
 
    gl_start(canvas, this);
@@ -36,17 +36,14 @@ function WebglCard(ctx) {
          newScene = new Function('M.identity();' + src);
       } catch {
          error => console.log('webgl compile error:', error);
-	 return;
+         return;
       }
       scene = newScene;
    }
 
    let startTime = Date.now()/1000;
 
-   this.setScene(`
-      M.turnY(time).scale(.6);
-      draw(cube, [0,.8,1]);
-   `);
+   this.setScene('M.turnY(time).scale(.6).draw(cube,[0,.8,1]);');
 
    let _I = [];
 
@@ -55,32 +52,41 @@ function WebglCard(ctx) {
    this.draw = (x,y,w) => {
 
       if (scene) {
+         let b = ( 'add,cross,dot,ease,evalBezier,hex,mix,norm,'
+                 + 'normalize,resize,round,subtract,round2,'
+                 + 'transform' ).split(',');
+         let m = ( 'PI,abs,ceil,cos,exp,floor,' +
+                   'log,max,min,mod,pow,random,' +
+                   'round,sign,sin,sqrt,trunc' ).split(',');
          let v = [
-            '_I'        , _I,
-            'M'         , M,
-	    'ball'      , ball,
-	    'cube'      , cube,
-	    'tube'      , tube,
-            'draw'      , draw,
-            'time'      , Date.now()/1000 - startTime,
+            '_I'   , _I,
+            'M'    , M,
+            'ball' , ball,
+            'cube' , cube,
+            'tube' , tube,
+            'time' , Date.now()/1000 - startTime,
          ];
-	 for (let i = 0 ; i < v.length ; i+=2) window[v[i]] = v[i+1];
+         for (let i = 0 ; i < b.length ; i++ ) window[b[i]] = b[i];
+         for (let i = 0 ; i < m.length ; i++ ) window[m[i]] = Math[m[i]];
+         for (let i = 0 ; i < v.length ; i+=2) window[v[i]] = v[i+1];
 
-	 let isError = false;
-	 try {
+         let isError = false;
+         try {
             scene();
          }
-	 catch {
-	    error => console.log('webgl runtime error:', error);
-	    isError = true;
-	 }
-	 if (isError) {
-	    scene = lastSceneThatWorks;
-	    this.setScene(scene);
-	 }
-	 else
-	    lastSceneThatWorks = scene;
-	    
+         catch {
+            error => console.log('webgl runtime error:', error);
+            isError = true;
+         }
+         if (isError) {
+            scene = lastSceneThatWorks;
+            this.setScene(scene);
+         }
+         else
+            lastSceneThatWorks = scene;
+
+         for (let i = 0 ; i < b.length ; i++ ) delete window[b[i]];
+         for (let i = 0 ; i < m.length ; i++ ) delete window[m[i]];
          for (let i = 0 ; i < v.length ; i+=2) delete window[v[i]];
       }
 
---
+++ projects/0423/src/cubes.cgi
@@ -0,0 +1,11 @@
+M.turnY(time).turnX(time)
+for (let n = 0 ; n < 6 ; n++)
+   M.push()
+    .turnY(n < 4 ? PI*n/2 : 0)
+    .turnZ(n < 4 ? 0 : PI*n+PI/2)
+    .move(.6,0,0)
+    .scale(.2)
+    .draw(cube,[.5+.5*cos(3*n),
+                .5+.5*sin(5*n),
+                .5+.5*sin(7*n) ])
+    .pop();
